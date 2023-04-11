c72892248b9b4b23a6cb2f0b52d24ca30c670088
Nikolay Dorofeev <dorich2000@gmail.com>
2023-04-11 13:50:14 +0400

    Lintfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,39 @@
+/*
+ * Copyright 2023 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    presets: [require('@exactpro/docs-web-toolkit/tailwind.config')],
-    theme:{
-        extend:{
-            colors:{
-                primary:{
-                    DEFAULT: '#286455',
-                    '50': '#D1EDE6',
-                    '100': '#C3E7DE',
-                    '200': '#A6DBCE',
-                    '300': '#88D0BE',
-                    '400': '#6BC4AE',
-                    '500': '#4EB89E',
-                    '600': '#3F9E87',
-                    '700': '#34816E',
-                    '800': '#286455',
-                    '900': '#183C33'
-                    
-                }
-            }
+  presets: [require('@exactpro/docs-web-toolkit/tailwind.config')],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#286455',
+          50: '#D1EDE6',
+          100: '#C3E7DE',
+          200: '#A6DBCE',
+          300: '#88D0BE',
+          400: '#6BC4AE',
+          500: '#4EB89E',
+          600: '#3F9E87',
+          700: '#34816E',
+          800: '#286455',
+          900: '#183C33'
         }
+      }
     }
   }
-  
+}
