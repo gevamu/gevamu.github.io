f056e01bcbd8f27b98da7ea3c3589161a5d9ce54
d0rich <d0rich@users.noreply.github.com>
2023-04-19 10:38:05 +0000

    Automatic deploy: [Hotfix] Toolkit with types, prettier auto end of line c1c6d3b60ec2d5c7a8ecdf0d5cad2e4783dff1c3

--- _nuxt/Markdown.a103b06b.js
+++
@@ -1,1 +0,0 @@
-import s from"./ContentSlot.1a39cfe3.js";import{a as o,p,i as u,v as f}from"./entry.ccd854ff.js";import"./utils.c82d394e.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=f(),{between:n,default:a}=p(),r=u(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
