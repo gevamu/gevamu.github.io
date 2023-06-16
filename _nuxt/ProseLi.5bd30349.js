0d27e0d558d0c126c09d3b75e75328de46e33961
Hdd87 <Hdd87@users.noreply.github.com>
2023-06-16 10:42:42 +0000

    Automatic deploy: [DO-551] Configure github actions button (#12)
    * run npm ci, added links needed for github buttons
    
    * run lintfix c31c579a444f6cd0f2476ffd463e6c66eba515ae

---
+++ _nuxt/ProseLi.5bd30349.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.c27b6911.js";import{o as r,h as t,l as s}from"./entry.856b4718.js";import"https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";const n={};function c(e,l){return r(),t("li",null,[s(e.$slots,"default")])}const i=o(n,[["render",c]]);export{i as default};
