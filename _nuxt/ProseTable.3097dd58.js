4154a8c224f71724011c95e8b9d99e7bd30c47ba
d0rich <d0rich@users.noreply.github.com>
2023-06-26 13:51:22 +0000

    Automatic deploy: [DO-565] Update dependencies (#14)
    * Update toolkit
    
    * Remove workaround configs
    
    * Update secondary shades a535dadfa0db36f28cd10dacc68a1a9ab9b73b3d

---
+++ _nuxt/ProseTable.3097dd58.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.c27b6911.js";import{o as r,h as t,l as s}from"./entry.cf6765e0.js";import"https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";const a={};function l(e,n){return r(),t("table",null,[s(e.$slots,"default")])}const m=o(a,[["render",l]]);export{m as default};
--- _nuxt/ProseThead.07eafada.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper.c27b6911.js";import{o as r,h as t,l as s}from"./entry.bd4a98cc.js";import"https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";const a={};function n(e,c){return r(),t("thead",null,[s(e.$slots,"default")])}const d=o(a,[["render",n]]);export{d as default};
